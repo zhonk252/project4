804694dcefea54d82c6dd0b82d400e9dbb26dc82
KoreZhong252 <77712486+zhonk252@users.noreply.github.com>
2021-05-06 23:22:45 +0800

    final check 2

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ function addImage(x,y) {
 	var img = document.createElement("img");
 	img.src = `${brush}.gif`;
 	document.body.appendChild(img);
-    
+  
 	img.style.left =`${x-img.offsetWidth / 2}px`
-    img.style.top =`${y-img.offsetHeight / 14}px`
+  img.style.top =`${y-img.offsetHeight / 14}px`
 
 	var class_name = "foo";
 	img.setAttribute("class", class_name);
@@ -53,29 +53,10 @@ document.querySelector('.right').addEventListener('click', (event) => {
 
 
 
-  document.addEventListener('keyup', (event) => {
-          if (event.key === 'ArrowLeft') plusDivs(-1);
-          if (event.key === 'ArrowRight') plusDivs(1);
-      });
-
-
-// function bgSwitcher() {
-//   let currentBg = 0;
-//   const bgs = ['toilet.jpeg', 'Food.jpeg', 'trash.jpeg', 'bg4.png'];
-//   const swapLeft = () => {
-//       currentBg = (currentBg > 0) ? currentBg - 1 : bgs.length - 1;
-//       document.body.style.backgroundImage = `url("${bgs[currentBg]}")`;
-//   };
-//   const swapRight = () => {
-//       currentBg = (currentBg < bgs.length - 1) ? currentBg + 1 : 0;
-//       document.body.style.backgroundImage = bgs[currentBg];
-//   };
-//   document.addEventListener('keyup', (event) => {
-//       if (event.key === 'ArrowLeft') swapLeft();
-//       if (event.key === 'ArrowRight') swapRight();
-//   });
-// }
-
+document.addEventListener('keyup', (event) => {
+  if (event.key === 'ArrowLeft') plusDivs(-1);
+  if (event.key === 'ArrowRight') plusDivs(1);
+});
 
 
 document.documentElement.addEventListener('click', (event) => {
@@ -83,34 +64,34 @@ document.documentElement.addEventListener('click', (event) => {
 });
 
 window.addEventListener('keyup', (event) => {
-    if (event.isComposing || event.keyCode === 229) return;
-    if (event.key === '1' || event.code === 'Digit1') {
+  if (event.isComposing || event.keyCode === 229) return;
+  if (event.key === '1' || event.code === 'Digit1') {
         brush = 'F1';
-    }
+  }
 	if (event.key === '2' || event.code === 'Digit2') {
         brush = 'F2';
-    }
+  }
 	if (event.key === '3' || event.code === 'Digit3') {
         brush = 'F3';
-    }
+  }
 	if (event.key === '4' || event.code === 'Digit4') {
         brush = 'F4';
-    }
+  }
 	if (event.key === '5' || event.code === 'Digit5') {
         brush = 'F5';
-    }
+  }
 	if (event.key === '6' || event.code === 'Digit6') {
         brush = 'F6';
-    }
+  }
 	if (event.key === '7' || event.code === 'Digit7') {
         brush = 'F7';
-    }
+  }
 	if (event.key === '8' || event.code === 'Digit8') {
         brush = 'F8';
-    }
+  }
 	if (event.key === '9' || event.code === 'Digit9') {
         brush = 'F9';
-    }
+  }
 	result.innerText = "You just pressed: " + event.key;
 
 });
@@ -163,12 +144,11 @@ function myfunction9() {
 window.addEventListener('keyup', (event) => {
 	if (event.code === 'Backspace') {
 	  while (document.body.querySelector('.foo')) {
-    document.body.querySelector('.foo').remove();
+      document.body.querySelector('.foo').remove();
     }
     count = 0;
     button.innerHTML = "You are "  + count + " level of pollen allergy";
 	}
-
 });
 
 
